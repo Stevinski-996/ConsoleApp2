78bbc53ec7c0be40eeeacf3291d423b05a9d0015
Mustafa Al-Hinity <162119747+Stevinski-996@users.noreply.github.com>
2024-06-30 19:12:10 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World 2");
+            Console.WriteLine("Hello Github");
         }
     }
 }
